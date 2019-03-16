5d087e7ac2cf9d8d85ee98bbaad7c7e82eadbcb9
Michael Grosser <michael@grosser.it>
2019-03-16 14:47:21 -0700

    fix

--- find_anagrams_test.rb
+++ find_anagrams_test.rb
@@ -1,9 +1,20 @@
 require 'maxitest/autorun'
 load 'find_anagrams'
 
+def capture_stdout
+  old = $stdout
+  $stdout = StringIO.new
+  yield
+  $stdout.string
+ensure
+  $stdout = old
+end
+
 describe "#find_anagrams" do
   it "finds lots of words" do
-    words = find_anagrams("fear".chars)
-    words.must_equal ['fare']
+    capture_stdout do
+      words = find_anagrams("fear".chars)
+      words.must_equal ['fare']
+    end
   end
 end
