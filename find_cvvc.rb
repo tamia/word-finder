298f8e857b62ec5acdb5906a1cde8f1661154b45
Michael Grosser <michael@grosser.it>
2019-11-23 10:33:31 -0800

    improve cvvc

--- find_cvvc.rb
+++ find_cvvc.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
+#
+# hack your homework, search cvvc but you can pick your own vowels!
 
-VOWEL = 'aeoui'
+VOWEL = '[aeoui]'
+CONSONANT = '[^aeoui]'
 
-def find_cvvc
-  words = File.read("words.txt").split("\n")
-  puts "#{words.size} words -- #{words.first(5)}"
+def find_cvvc(middle)
+  all_words = File.read("words.txt").split("\n")
+  puts "#{all_words.size} words total -- #{all_words.first(5)}"
 
-  words = words.grep(/^[^#{VOWEL}]+[#{VOWEL}]{2}[^#{VOWEL}]+$/i)
-  puts "#{words.size} words -- #{words.first(5)}"
-  words
+  matched_words = all_words.grep(/#{CONSONANT}#{middle}#{CONSONANT}/)
+  puts "#{matched_words.size} words matched -- #{matched_words.first(5)}"
+
+  matched_words
 end
 
-puts find_cvvc unless $0.include?("_test.rb")
+puts find_cvvc(ARGV[0] || "#{VOWEL}#{VOWEL}") unless $0.include?("_test.rb")
